1a8030e2c884c082b687e4201ba55c29d284dd63
Sarah Randles <sarrandles@gmail.com>
2017-12-07 12:09:59 -0500

    added functionality and wins and percentages

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-var heads = 0;
-var tails = 0;
+var floops = 0;
+var correct = 0;
+var totalPercent = 0;
 
 function toss() {
-    coin = (Math.floor(Math.random() * 2) == 0);
-    if(coin){
+    coin = (Math.floor(Math.random() * 2) + 1);
+    if (coin === 1) {
         flip("heads");
-    }else{
+    } else if (coin === 2) {
         flip("tails");
     }
 }
@@ -18,16 +19,28 @@ function headsChoice() {
     toss();
     document.getElementById("guess").innerHTML = "heads";
     document.getElementById("result");
+    if (coin === 1) {
+        correct += 1;
+        document.getElementById("correct").innerHTML = correct;
+    }
 }
 
 function tailsChoice() {
     toss();
     document.getElementById("guess").innerHTML = "tails";
     document.getElementById("result");
+    if (coin === 2) {
+        correct += 1;
+        document.getElementById("correct").innerHTML = correct;
+    }
 }
 
+function psyPercent() {
+    totalPercent = correct / floops * 100;
+    document.getElementById("psyPercent").innerText = Math.round(totalPercent);
+}
 
-/*
-    Need to:
-        1.) heads or tails selection connects to the buttons
-*/
+function totalFloops() {
+    floops += 1;
+    document.getElementById("totalFloops").innerHTML = floops;
+}
